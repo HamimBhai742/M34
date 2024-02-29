d17b1345ca18c453c4bcb38d41504ff1277c9728
HamimBhai742 <omagootruelove147@gmail.com>
2024-02-29 23:01:51 +0600

    kk

--- js/javascript.js
+++ js/javascript.js
@@ -8,10 +8,19 @@ const phoneLoad = async (inputText, isShowAll) => {
 // phoneLoad()
 
 const displyPhone = (phoneData, isShowAll) => {
-
+    console.log(phoneData.length);
     const phoneConatner = document.getElementById('phone-container')
     phoneConatner.textContent = ''
     // console.log(phoneData.length);
+    const noDataFound=document.getElementById('no-data-found')
+    const noContainer=document.getElementById('no-data-container')
+    if(phoneData.length<=0){
+        noDataFound.innerText='No Data Found!'
+        noContainer.classList.remove('hidden')
+    }
+    else{
+        noContainer.classList.add('hidden')
+    }
     console.log('rrrrrrrrrex v dfv dvdf vfdf rt:', isShowAll);
     const showBtn = document.getElementById('show-all-btn')
     if (phoneData.length > 12 && !isShowAll) {
@@ -74,15 +83,15 @@ const showDetlisBtn = async (id) => {
     const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`)
     const data = await res.json()
     // console.log(data);
-    const phones=data.data
+    const phones = data.data
     modalBtn(phones)
 }
 
 const modalBtn = (phones) => {
     shodetlis_btn_modal.showModal(phones)
-    const modalContainer=document.getElementById('modal-container')
+    const modalContainer = document.getElementById('modal-container')
     // const modalDiv=document.createElement('div')
-    modalContainer.innerHTML=`
+    modalContainer.innerHTML = `
     <figure class="bg-[#0D6EFD0D] p-3 rounded-none flex justify-center"><img src="${phones.image}" alt="Shoes" />
         </figure>
     <h3 class="mt-3 text-3xl font-bold text-[#403F3F]">${phones.name}</h3>
@@ -92,7 +101,7 @@ const modalBtn = (phones) => {
     <p class="text-xl text-[#706F6F] mt-3"><span class="font-semibold text-[#403F3F]">Chipset:</span>${phones.mainFeatures.chipSet}</p>
     <p class="text-xl text-[#706F6F] mt-3"><span class="font-semibold text-[#403F3F]">Memory:</span>${phones.mainFeatures.memory}</p>
     <p class="text-xl text-[#706F6F] mt-3"><span class="font-semibold text-[#403F3F]">Slug:</span>${phones.slug}</p>
-    <p class="text-xl text-[#706F6F] mt-3"><span class="font-semibold text-[#403F3F]">Release data:</span>${phones?.releaseDate }</p>
+    <p class="text-xl text-[#706F6F] mt-3"><span class="font-semibold text-[#403F3F]">Release data:</span>${phones?.releaseDate}</p>
     <p class="text-xl text-[#706F6F] mt-3"><span class="font-semibold text-[#403F3F]">Brand:</span>${phones.brand}</p>
     <p class="text-xl text-[#706F6F] mt-3"><span class="font-semibold text-[#403F3F]">GPS:</span>${phones?.others?.GPS || 'No GPS avalible'}</p>
 
